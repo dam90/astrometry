f2b1b3a4604b3210c6923d3e47d6ca499b260220
davidmedearis <davidmedearis@gmail.com>
2018-03-26 15:19:17 -0400

    update compile for single core

--- base/install_dependencies.sh
+++ base/install_dependencies.sh
@@ -39,6 +39,9 @@ yum install -y bzip2-devel \
                python \
                swig.x86_64
 
+
+echo yes | pip install numpy
+
 # install latest cfitsio
 CFITS_URL=http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio_latest.tar.gz
 curl -L  $CFITS_URL > cfitsio.tar.gz
@@ -59,8 +62,6 @@ pip install fitsio astropy
 yum install -y subversion tkinter
 
 # install NOVA python dependencies:
-echo yes | pip install numpy
-
 echo yes | pip install setuptools\
                       wheel \
                       fitsio \
